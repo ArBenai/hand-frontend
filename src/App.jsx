56b19e9ba909232704ba5ad3480547c6e904f95c
volltoll8@gmx.de <volltoll8@gmx.de>
2025-06-30 15:17:31 +0200

    Exchange-unterseiten

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,3 @@
-// client/src/App.jsx
-
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Navbar from './components/Navbar';
@@ -7,28 +5,34 @@ import LandingPage from './pages/LandingPage';
 import Login from './pages/Login';
 import Register from './pages/Register';
 import Profile from './pages/Profile';
-import Exchange from './pages/Exchange/Exchange.jsx'; // Importiere die Exchange-Komponente
-import Help from './pages/Help'; 
-
-
+import Home from './pages/Home';
+import Events from './pages/Events';
+import EventDetail from './pages/EventDetail.jsx';
+import EventRegister from './pages/EventRegister.jsx';
+import Help from './pages/Help';
+import Exchange from './pages/Exchange/Exchange';
+import ForgotPassword from './components/ForgotPassword';
 function App() {
   return (
     <Router>
       <Navbar /> {/* Die Navbar wird immer angezeigt, unabhängig von der Route */}
       <div className="container"> {/* Optional: Ein Container für deinen Seiteninhalt */}
         <Routes>
-          <Route path="/" element={<LandingPage/>} /> 
+          <Route path="/" element={<LandingPage/>} />
           <Route path="/login" element={<Login />} />
           <Route path="/register" element={<Register />} />
-          <Route path="/help" element={<Help/>} />
-          <Route path="/events" />
+          <Route path="/home" element={<Home />} />
+          <Route path="/home/:id" element={<Home />} />
+          <Route path="/help" element={<Help />} />
+          <Route path="/events" element={<Events />} />
+          <Route path="/events/:id" element={<EventDetail />} />
+          <Route path="/events/:id/register" element={<EventRegister />} />
           <Route path="/profile" element={<Profile />} />
-          <Route path="/exchange/*" element={<Exchange />} /> {/* exchange beinhaltet: Verschenke, Tauschen, Suchen */}
-
+          <Route path="/exchange/*" element={<Exchange />} />
+          <Route path="/forgot-password" element={<ForgotPassword />} />
         </Routes>
       </div>
     </Router>
   );
 }
-
 export default App;
--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App.jsx'; // Wir gehen davon aus, dass App.jsx existiert
+import { AuthProvider } from './pages/Exchange/AuthContext.jsx';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <AuthProvider>
+      <App />
+    </AuthProvider>
   </React.StrictMode>,
 );
