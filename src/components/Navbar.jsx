0d9b9f9e72f1af4cd4bab2620ee5013ce5d9b917
ArBenai <arben.musa@dci-student.org>
2025-07-08 14:06:39 +0200

    new

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -33,8 +33,6 @@ function Navbar() {
                 <a href="/home" className="nav-links">
                   Home 
                 </a>
-                <a href="/" className="nav-links">
-                  Home </a>
               </li>
               <li className='nav-item'>
                 <a href="/exchange" className="nav-links">
